8cec855ffc7ccdc27720b94cee9f8c1912e3e23d
chris097 <chrisfidel.international@gmail.com>
2023-02-18 18:29:28 +0100

    feature header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <div className='h-20 bg-white shadow flex justify-between items-center px-20'>
+          <div>Logo</div>
+          <div>dark mode</div>
+    </div>
+  )
+}
+
+export default Header
